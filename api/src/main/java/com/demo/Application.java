16655b8653b194d56579868413eff63d3e5a109a
hanlipeng <hanlipeng@nibirutech.com>
2018-10-10 13:12:33 +0800

    首次提交

---
+++ api/src/main/java/com/demo/Application.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hanlipeng
+ * @date 2018/10/10
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ api/src/main/java/com/demo/config/DataConfig.java
@@ -0,0 +1,32 @@
+package com.demo.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * @author hanlipeng
+ * @date 2018/10/10
+ */
+@Configuration
+public class DataConfig {
+    @Bean(destroyMethod = "close")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(druidDataSource());
+        return jdbcTemplate;
+    }
+}
